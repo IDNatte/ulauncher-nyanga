acb53223939f5a0a356aa87f58cdb0955ab57355
IDNatte <idnatte@gmail.com>
2024-05-20 09:24:47 +0800

    disable download image cause it too big, slowing searching in ulauncher

--- main.py
+++ main.py
@@ -1,5 +1,110 @@
+# import logging
+# import subprocess
+
+# from ulauncher.api.client.EventListener import EventListener
+# from ulauncher.api.client.Extension import Extension
+# from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+# from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+# from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+# from ulauncher.api.shared.event import ItemEnterEvent, KeywordQueryEvent
+# from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+
+# from src.constant import THUMB_PATH
+# from src.result_composer import composer
+# from src.tempfile_man import create_temp, remove_temp
+# from src.thumb_dl import download_image
+
+# logger = logging.getLogger(__name__)
+
+
+# class DemoExtension(Extension):
+#     def __init__(self):
+#         super().__init__()
+#         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+#         self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
+
+# class ItemEnterEventListener(EventListener):
+#     def on_event(self, event, extension):
+#         logger.info(f"\n\n{event.get_data()}\n\n")
+#         data = event.get_data()
+
+#         if data.get("open_app"):
+#             subprocess.run([data.get("nyr_bin")])
+
+#         else:
+#             subprocess.run(
+#                 [
+#                     data.get("nyr_bin"),
+#                     "--extension",
+#                     "--context",
+#                     "open_manga",
+#                     "--manga-id",
+#                     data.get("mangaid"),
+#                 ],
+#             )
+
+#         # remove_temp()
+
+#         return RenderResultListAction(
+#             [
+#                 ExtensionResultItem(
+#                     icon="images/icon.png",
+#                     name=data.get("manga_name"),
+#                     on_enter=HideWindowAction(),
+#                 )
+#             ]
+#         )
+
+
+# class KeywordQueryEventListener(EventListener):
+#     def on_event(self, event, extension):
+#         # create_temp()
+#         data = [
+#             ExtensionResultItem(
+#                 icon="images/icon.png",
+#                 name="Open Nyanga Read",
+#                 description="Open Nyanga-Read Application",
+#                 on_enter=ExtensionCustomAction(
+#                     {
+#                         "open_app": True,
+#                         "nyr_bin": "{0}".format(
+#                             extension.preferences["nyanga_bin_path"]
+#                         ),
+#                         "mangaid": None,
+#                         "manga_name": None,
+#                     },
+#                     keep_app_open=True,
+#                 ),
+#             )
+#         ]
+
+#         if event.get_argument():
+#             result = subprocess.run(
+#                 [
+#                     extension.preferences.get("nyanga_bin_path"),
+#                     "--extension",
+#                     "--context",
+#                     "search_manga",
+#                     "--manga-name",
+#                     event.get_argument(),
+#                 ],
+#                 capture_output=True,
+#                 text=True,
+#             )
+
+#             composer(result, data, extension)
+
+#         return RenderResultListAction(data)
+
+
+# if __name__ == "__main__":
+#     DemoExtension().run()
+
 import logging
+import queue
 import subprocess
+import threading
 
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.client.Extension import Extension
@@ -29,22 +134,21 @@ class ItemEnterEventListener(EventListener):
         logger.info(f"\n\n{event.get_data()}\n\n")
         data = event.get_data()
 
-        if data.get("open_app"):
-            subprocess.run([data.get("nyr_bin")])
+        command = [data.get("nyr_bin")]
 
-        else:
-            subprocess.run(
+        if not data.get("open_app"):
+            command.extend(
                 [
-                    data.get("nyr_bin"),
                     "--extension",
                     "--context",
                     "open_manga",
                     "--manga-id",
                     data.get("mangaid"),
-                ],
+                ]
             )
 
-        remove_temp()
+        process = subprocess.Popen(command)
+        process.wait()
 
         return RenderResultListAction(
             [
@@ -59,7 +163,7 @@ class ItemEnterEventListener(EventListener):
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
-        create_temp()
+        # create_temp()
         data = [
             ExtensionResultItem(
                 icon="images/icon.png",
@@ -68,7 +172,7 @@ class KeywordQueryEventListener(EventListener):
                 on_enter=ExtensionCustomAction(
                     {
                         "open_app": True,
-                        "nyr_bin": f"{extension.preferences['nyanga_bin_path']}",
+                        "nyr_bin": extension.preferences["nyanga_bin_path"],
                         "mangaid": None,
                         "manga_name": None,
                     },
@@ -78,22 +182,62 @@ class KeywordQueryEventListener(EventListener):
         ]
 
         if event.get_argument():
-            result = subprocess.run(
-                [
-                    extension.preferences.get("nyanga_bin_path"),
-                    "--extension",
-                    "--context",
-                    "search_manga",
-                    "--manga-name",
-                    event.get_argument(),
-                ],
-                capture_output=True,
-                text=True,
+            command = [
+                extension.preferences.get("nyanga_bin_path"),
+                "--extension",
+                "--context",
+                "search_manga",
+                "--manga-name",
+                event.get_argument(),
+            ]
+
+            def enqueue_output(pipe, queue):
+                for line in iter(pipe.readline, ""):
+                    queue.put(line)
+                pipe.close()
+
+            process = subprocess.Popen(
+                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
             )
 
-            composer(result, data, extension)
+            stdout_queue = queue.Queue()
+            stderr_queue = queue.Queue()
 
-            # logger.info(f"\n\n{c}\n\n")
+            stdout_thread = threading.Thread(
+                target=enqueue_output, args=(process.stdout, stdout_queue)
+            )
+            stderr_thread = threading.Thread(
+                target=enqueue_output, args=(process.stderr, stderr_queue)
+            )
+
+            stdout_thread.start()
+            stderr_thread.start()
+
+            # Wait for the process to complete
+            process.wait()
+
+            # Ensure threads have finished
+            stdout_thread.join()
+            stderr_thread.join()
+
+            # Collect all output
+            stdout_lines = []
+            stderr_lines = []
+
+            while not stdout_queue.empty():
+                stdout_lines.append(stdout_queue.get())
+
+            while not stderr_queue.empty():
+                stderr_lines.append(stderr_queue.get())
+
+            stdout_output = "".join(stdout_lines)
+            stderr_output = "".join(stderr_lines)
+
+            if stderr_output:
+                logger.error(stderr_output)
+
+            # logger.info("\n\n{0}\n\n".format(stdout_output))
+            composer(stdout_output, data, extension)
 
         return RenderResultListAction(data)
 
--- src/result_composer.py
+++ src/result_composer.py
@@ -1,53 +1,34 @@
-import concurrent.futures as fu
 import json
 import re
 
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
-from .constant import THUMB_PATH
-from .thumb_dl import download_image
-
 
 def composer(data, init_data, extension):
-    data_match = re.search(r"\[.*\]", data.stdout, re.DOTALL)
+    data_match = re.search(r"\[\s*{.*}\s*\]", data, re.DOTALL)
     if data_match:
         data_json_res = json.loads(data_match.group(0))
         for x in data_json_res:
-            cover_art_items = [
-                (x.get("id"), y.get("attributes").get("fileName"))
-                for y in x.get("relationships")
-                if y.get("type") == "cover_art"
-            ]
-
-            with fu.ThreadPoolExecutor() as downloader:
-                fu_data = [
-                    downloader.submit(download_image, id, file_name)
-                    for id, file_name in cover_art_items
-                ]
-                fu.wait(fu_data)
+            # cover_art_items = [
+            #     (x.get("id"), y.get("attributes").get("fileName"))
+            #     for y in x.get("relationships")
+            #     if y.get("type") == "cover_art"
+            # ]
 
             init_data.append(
                 ExtensionResultItem(
-                    icon="{0}/{1}".format(
-                        THUMB_PATH,
-                        next(
-                            (
-                                rel.get("attributes").get("fileName")
-                                for rel in x.get("relationships")
-                                if rel.get("type") == "cover_art"
-                            ),
-                            "../images/icon.png",
-                        ),
-                    ),
-                    name=x.get("attributes").get("title").get("en"),
+                    icon="images/icon.png",
+                    name=x.get("attributes").get("title").get("en") or "No title",
                     description="Read {0}".format(
                         x.get("attributes").get("title").get("en")
                     ),
                     on_enter=ExtensionCustomAction(
                         {
                             "open_app": False,
-                            "nyr_bin": f"{extension.preferences['nyanga_bin_path']}",
+                            "nyr_bin": "{0}".format(
+                                extension.preferences["nyanga_bin_path"]
+                            ),
                             "mangaid": x.get("id"),
                             "manga_name": x.get("attributes").get("title").get("en"),
                         },
